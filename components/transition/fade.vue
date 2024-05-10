1073e6a5101107aca9677d30429b2bdda1c4a4dc
AKuchko <vasco_da_gama1469@mail.ru>
2024-05-10 12:03:33 +0700

    feat(transition-fade)

---
+++ components/transition/fade.vue
@@ -0,0 +1,24 @@
+<template>
+  <transition name="fade" mode="out-in">
+    <slot />
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'transition-fade',
+};
+</script>
+
+<style lang="scss">
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.25s ease;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  scale: 0.95;
+}
+</style>
